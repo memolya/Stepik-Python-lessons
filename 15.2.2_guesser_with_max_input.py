b6c1c96adc291cc604144d3793a987a1bd83a568
Helga <semenishevaolchik@yandex.ru>
2024-06-06 00:09:01 +0300

    refactor and update

--- 15.2.2_guesser_with_max_input.py
+++ 15.2.2_guesser_with_max_input.py
@@ -1,26 +1,32 @@
 from random import *
 
-
-        return True
-    return False
-
-
 def is_valid(num):
-    if num.isdigit() and 0 < int(num) <= n:
+    if num.isdigit() and 0 < int(num) <= max_num:
         return True
     return False
 
+def more():
+    print('Играть дальше? да/нет ')
+    answer = input()
+    if answer == 'да':
+        guess_number()
+
+    else:
+        print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+        exit() #interrupt the whole code if 'no' enters
 
 def guess_number():
-
-    print('Компьютер загадал число от 1 до ' + str(n) + ' Попробуйте угадать!')
+    global max_num
+    max_num = int(input('Введите максимальное возможное число: '))
+    random_num = randint(1, max_num)
+    print('Компьютер загадал число от 1 до ' + str(max_num) + '. ' + 'Попробуйте угадать!')
     attempts = 0
 
     while True:
         input_guess = input("Введите вашу догадку: ")
 
         if not is_valid(input_guess):
-            print('А может быть все-таки введем целое число от 1 до 100?')
+            print('А может быть все-таки введем целое число от 1 до ' + str(max_num) + '?')
             continue
 
         guess = int(input_guess)
@@ -31,23 +37,8 @@ def guess_number():
         elif guess > random_num:
             print("Загаданное число меньше вашей догадки.")
         else:
-            print(
-                f"Поздравляем! Вы угадали число {random_num} за {attempts} попыток!", end = '\n'
-                'Играть дальше? да/нет ')
+            print(f"Поздравляем! Вы угадали число {random_num} за {attempts} попыток!", end = '\n')
+            more()
 
-            answer = input()
-            if answer == 'да':
-                guess_number()
-                print('Играть дальше? да/нет ')
-
-            else:
-                print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
-            break
-
-print('Введите целое число для определения максимального возможного числа: ')
-n = int(input())
-if n.isdigit() and int(n) > 0:
-    random_num = randint(1, n + 1)
 
 guess_number()
-
